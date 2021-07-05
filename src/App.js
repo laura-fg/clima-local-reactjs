b92a8285455f231565339a435dc646f862d01c73
laura <lauura_1998_@hotmail.com>
2021-07-05 16:45:34 -0300

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+import React, {Fragment, useState, useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
+import climaTempo from './img/climaTempo.png';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [ location, setLocation ] = useState(false);
+  const [ weather, setWeather ] = useState(false);
+
+  let getWeather = async (lat, long) => {
+    let res = await axios.get("http://api.openweathermap.org/data/2.5/weather", {
+      params: {
+        lat: lat,
+        lon: long,
+        appid: process.env.REACT_APP_OPEN_WHEATHER_KEY,
+        lang: 'pt',
+        units: 'metric'
+      }
+    })
+    setWeather(res.data);
+  }
+
+
+  useEffect(() =>{
+   navigator.geolocation.getCurrentPosition((position) => {
+     getWeather(position.coords.latitude, position.coords.longitude);
+     setLocation(true);
+   })
+   }, [])
+
+
+   if(location == false) {
+     return(
+      <div className="box">  <div className="background-1">É NECESSÁRIO PERMITIR ACESSO A SUA LOCALIZAÇÃO </div></div>
+     )}
+     else if(weather == false) {
+       return(
+        <div className="box">  <div className="background-1"> <img src={climaTempo}/> AGUARDE...... </div></div>
+       )
+     }
+     else{
+      return (
+        <Fragment>
+          <div className="box">
+          <div className="background-1">
+         <div> <img src={climaTempo}/> <h3> Como está o clima hoje em suas coordenadas: ({weather['weather'][0]['description']})</h3></div>
+         <ul>
+            <li> Temperatura atual: {weather['main']['temp']}°</li>
+            <li>Temperatura máxima: {weather['main']['temp_max']}°</li>
+            <li>Temperatura minima: {weather['main']['temp_min']}°</li>
+            <li>Pressão: {weather['main']['pressure']} hpa</li>
+            <li>Umidade: {weather['main']['humidity']}%</li>
+         </ul>
+         </div>
+         </div>
+        </Fragment>
+      );
+    }
+     }
+   
+  
 
 export default App;
